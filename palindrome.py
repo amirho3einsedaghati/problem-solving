eb18723696d53388a8671549282009dcbb0a8cec
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-10-09 19:24:46 +0330

    modify palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -9,13 +9,4 @@ def is_palindrome(string):
 # print(is_palindrome('wassaw'))
 # print(is_palindrome("abcddcbd"))
 
-# ------------------------------------
-# baraye maghloob kardan(ya invert kardan) yek reshte ya list, mitavan az dastresi be item az tarighe index estefade kard.
-# string= "amiimd"
-# invert= string[::-1]
-# print(invert)
 
-# li= [12,4,5,12,6,12]
-# invert_li= li[::-1]
-# print(invert_li)
-# print(li[1:4:])
