1f2eba8ee3e8498bc06db5f2e9571baa2fee041b
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-10-09 19:26:57 +0330

    final modify palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,5 @@
-# Palindrome(yani az do sar): 
-# tarif: age az do sare yek ebarat harekat konim va be yek chiz beresim, mitavan goft an ebarat(yani oon reshte, oon list); yek palindrome ast.
+# Stirng Palindrome: 
+
 
 def is_palindrome(string):
     if string == string[::-1]:
