383bbcad495e54a4ee870fa8d286bac1044a3969
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-10-26 23:44:21 +0330

    add reverse method

--- stack_class.py
+++ stack_class.py
@@ -4,36 +4,44 @@
 class stack:
     def __init__(self):
         self.__list= []
-    
+
 
     @property
     def stack(self):
+        # O(1)
         return self.__list
 
 
     @stack.setter
-    def stack(self, string):
-        self.__list.append(string)
+    def stack(self, vacentString):
+        # O(1)
+        self.__list.append(vacentString)
 
 
     def push(self, item):
+        # O(1)
         self.stack= ''
         last= len(self.__list) - 1
         self.__list[last]= item
 
 
     def pop(self):
+        # O(1)
         li= []
         count= len(self.__list)
+        i= 0
+        while i != count - 1:
+            li.append(self.__list[i])
+            i += 1
 
-        for i in range(0, count):
-            if i != count - 1:
-                li.append(self.__list[i])
-        
+        backup= self.__list
         self.__list= li
+        return backup[i]
+        
 
 
     def peek(self):
+        # O(1)
         count= len(self.__list)
 
         for i in range(0, count):
@@ -42,9 +50,29 @@ class stack:
     
 
     def isEmpty(self):
+        # O(1)
         return self.__list == []
 
+
+    def reverse(self, string):
+        # O(n)
+        self.__list= []
+        for item in string:
+            self.push(item)
+
+        li= []
+        for i in range(len(self.__list) - 1, -1, -1): # this loop is for deleting the items
+            li.append(self.pop())
+            
+        reversed= ""
+        for i in range(0, len(li)):
+            reversed += "".join(li[i])
+
+        return reversed
+
+
 # ================================================ create some objects ====================================
+
 
 obj= stack()
 
@@ -58,8 +86,15 @@ print(obj.stack)
 print(obj.peek())
 print(obj.isEmpty())
 
-obj.pop()
+x= obj.pop()
 print(obj.stack)
+print(x) # 30
 
 print(obj.peek())
 print(obj.isEmpty())
+
+print(obj.reverse("amirali"))
+print(obj.stack)
+obj.push(100)
+obj.push(200)
+print(obj.stack)
