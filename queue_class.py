3ce32e8b3208798d8c68b3760265c6b3db24ec1d
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-11-07 19:31:51 +0330

    complete documents and fix reverse method

--- queue_class.py
+++ queue_class.py
@@ -1,4 +1,5 @@
 # ======================================================== Implementation queue ===========================================
+# implement a queue by an dynamic array
 
 
 class Queue:
@@ -18,14 +19,14 @@ class Queue:
         self.__list.append(val)
 
 
-    def add(self, item): # adds an item to the end of the queue
+    def add(self, item): # add method is like enqueue that adds an item to the end of the queue
         # big O: O(1)
         self.queue= ""
         self.__end += 1
         self.__list[self.__end]= item
 
 
-    def remove(self): # deletes an item from the front of the queue and returns the value of it
+    def remove(self): # remove method is like dequeue that deletes an item from the front of the queue and returns the value of it
         # big O: O(1)
         if self.isEmpty():
             return "queue is empty"
@@ -47,18 +48,19 @@ class Queue:
         return front
 
 
-    def isEmpty(self):
+    def isEmpty(self): # checks queue is empty or not 
         # big O: O(1)
         return self.__list == []
 
 
-    def reverse(self):
+    def reverse(self): # reverses the items of queue, and returns the reversed queue for us
         # big O: O(n)
         li= []
         for i in range(len(self.__list) - 1, -1, -1):
             li.append(self.__list[i])
         
-        return li
+        self.__list= li
+        return self.__list
 
     
 # ======================================================== Create some objects ===========================================
