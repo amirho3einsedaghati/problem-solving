fd76afa9c56ae012b255d3edc04e6e11a9031c6a
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-11-10 01:25:27 +0330

    delete the additional codes

--- priority_queue_with_array.py
+++ priority_queue_with_array.py
@@ -8,9 +8,7 @@ class priorityQueue:
     def __init__(self, arrayCapacity):
         self.__list= [0] * arrayCapacity
         self.__countter= 0
-        # self.__start= -1
-        # self.__end= -1
-
+        
 
     @property
     def Queue(self):
@@ -36,6 +34,7 @@ class priorityQueue:
         lis= []
         min= li[0]
         lis.append(min)
+
         i= 1
         while i < len(li):
             if li[i] <= min:
@@ -52,16 +51,6 @@ class priorityQueue:
 
 
     def __displacement_items(self, l: list):
-        # lis= l
-        # length= len(lis)
-        # for i in range(length - 1, -1, -1):
-        #     if lis[i] < lis[i - 1]:
-        #         copy= lis[i - 1]
-        #         lis[i - 1]= lis[i]
-        #         lis[i]= copy
-
-        # return lis
-
         lis= l
         length= len(lis) - 1
 
