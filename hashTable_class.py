fd5f0a64bb5d59e4506952de51b21511a8a82492
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-11-20 19:39:35 +0330

    refactor the codes , add the __getBucketIndex, __getBucketNode, __getlastNodeValue methods

--- hashTable_class.py
+++ hashTable_class.py
@@ -31,6 +31,54 @@ class HashTable:
         self.__lisTuple= []
 
 
+    def update(self, key, value):
+        bucketIndex= self.__getBucketIndex(key)
+        bucketNode= self.__getBucketNode(bucketIndex)
+
+        if bucketNode == None:
+            self.__buckets[bucketIndex]= Node(key, value) # Create node, add it, return
+            return
+
+        else:
+	        while bucketNode is not None: # Collision! Iterate to the end of the linked list at provided index
+		        currentNode = bucketNode
+		        bucketNode = bucketNode._Node__address 
+
+	        currentNode._Node__address = Node(key, value) # Add a new node at the end of the list with provided key/value
+
+
+    def getValue(self, key):
+        bucketIndex= self.__getBucketIndex(key)
+        bucketNode= self.__getBucketNode(bucketIndex)
+
+        if bucketNode == None:
+            return
+
+        elif bucketNode._Node__address == None:
+            return bucketNode._Node__value 
+
+        lastNode= self.__getlastNodeValue(bucketNode)
+        return lastNode._Node__value
+
+
+    def remove(self, key):
+        bucketIndex= self.__getBucketIndex(key)
+        bucketNode= self.__getBucketNode(bucketIndex)
+
+        lastNode= self.__getlastNodeValue(bucketNode)
+
+        dictionary= dict(self.__lisTuple)
+        listDict= list(dictionary.items())
+
+        for tuple in listDict:
+            if tuple[1] == lastNode._Node__value:
+                listDict.remove(tuple)
+                self.__lisTuple= listDict
+                break
+
+        return lastNode._Node__value
+
+
     def __getlistofTuple(self):
         for item in self.__buckets:
 
@@ -44,7 +92,6 @@ class HashTable:
         return dict(self.__lisTuple)
         
         
-
     def __getlistOfList(self): # this method has came beacause see what self.__buckets looks like
         lis= self.__getlistofTuple()
 
@@ -67,58 +114,20 @@ class HashTable:
         return key % len(self.__buckets)
 
 
-    def update(self, key, value):
-	    index = self.__hashFunction(key) # Compute index of key
-	    nodeLL = self.__buckets[index]
-
-	    if nodeLL is None: # If bucket is empty:
-		    self.__buckets[index] = Node(key, value) # Create node, add it, return
-		    return
-        
-	    while nodeLL is not None: # Collision! Iterate to the end of the linked list at provided index
-		    currentNode = nodeLL
-		    nodeLL = nodeLL._Node__address 
-
-	    currentNode._Node__address = Node(key, value) # Add a new node at the end of the list with provided key/value
+    def __getBucketIndex(self, key: int):
+        return self.__hashFunction(key) # Compute index of key
 
 
-    def getValue(self, key):
-        index= self.__hashFunction(key)
-
-        targetNode= self.__buckets[index]
-
-        if targetNode == None:
-            return
-
-        elif targetNode._Node__address == None:
-            return targetNode._Node__value 
-
-        while targetNode != None:
-            curentNode= targetNode
-            targetNode= targetNode._Node__address
-
-        return curentNode._Node__value 
+    def __getBucketNode(self, index: int):
+        return self.__buckets[index]
 
 
-    def remove(self, key):
-        index= self.__hashFunction(key)
+    def __getlastNodeValue(self, node: Node):
+        while node != None:
+            curentNode= node
+            node= node._Node__address
 
-        targetNode= self.__buckets[index]
-
-        while targetNode._Node__address != None:
-            targetNode= targetNode._Node__address
-
-        dictionary= dict(self.__lisTuple)
-
-        listDict= list(dictionary.items())
-
-        for tuple in listDict:
-            if tuple[1] == targetNode._Node__value:
-                listDict.remove(tuple)
-                self.__lisTuple= listDict
-                break
-
-        return targetNode._Node__value
+        return curentNode
 
 
 # ========================================== create some objects ================================
