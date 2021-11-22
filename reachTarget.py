b786fb8596c06d717b0686ab672aa5613182848e
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-11-22 15:07:02 +0330

    add methods and edge-cases

---
+++ reachTarget.py
@@ -0,0 +1,41 @@
+# ========================================= the sum of the indices values, bring us to the target value ========================================
+
+# problem: if given you an array of the integer number and target value, create a program that can find the values of the indices 
+# That the sum of them, bring us to the target value.
+
+
+def reachTarget(li: list[int], target: int):
+
+    uniqueList= list()
+    
+    for item in li:
+        if item not in uniqueList:
+            uniqueList.append(item)
+
+    indicesList= []
+    copy= None
+
+    for i in range(0, len(uniqueList) - 1):
+        reachTargetVal= target - uniqueList[i]
+
+        if reachTargetVal in uniqueList and i > uniqueList.index(reachTargetVal):
+            reachTargetIndex= uniqueList.index(reachTargetVal)
+            indicesList.append(reachTargetIndex)
+            indicesList.append(i)
+
+            print(indicesList, end= " ")
+
+            copy= indicesList.copy
+            indicesList.clear()
+
+    if copy == None:
+        print(f"can't reach to the {target}")
+
+    print('\n')
+
+
+
+reachTarget([2,7,2,7,11,13], 9)
+reachTarget([2,7,3,6,11,13], 9)
+reachTarget([1,2,11,13], 9)
+
